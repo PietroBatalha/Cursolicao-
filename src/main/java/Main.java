8fb01364d9438de01ec0fac9d3b636f3f2aeca76
PietroBatalha <169068274+PietroBatalha@users.noreply.github.com>
2024-05-13 21:52:26 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+     System.out.println("#####");
+    System.out.println("Curso Opinião");
+        System.out.println("#####");
+
+        System.out.println(" ");
+
+        System.out.println("O curso. Está bem interresante, achei bem legal sobre a aprendizagem... principalmente agora! contém ensinamento na prática agora(mesmo que estejamos no começo)e quero aprender cada vez mais, enfim. o curso até agora não está me decepcionando... está bem legal de aprender e praticar!");
+    
+    
+    
+  }
+  
+}
